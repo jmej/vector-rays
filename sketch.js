102195b5150e7314bc9d8b50bd1400624e1e4e5e
jmej <jmejia@anestheticaudio.com>
2025-03-06 11:43:47 -0800

    basic ray example

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800 , 800);
+  background(255);
+}
+
+function draw() {
+  
+  translate(width/2, height/2);
+  let v = p5.Vector.random2D();
+  v = v.mult(random(50, 200))
+  strokeWeight(4);
+  stroke(0, 50);
+  line(0, 0, v.x, v.y);
+
+}
